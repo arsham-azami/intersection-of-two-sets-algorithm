d153f50f1ce65f3e61e50c3868aca9a26d69d6e8
arsham-azami <arsham-azami>
2020-08-29 12:15:55 +0430

    finish

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let arrOne = [43, 65, 21, 65, 76]
-let arrTwo = [90, 65, 98, 60, 23]
-
-if(arrTwo.includes(arrOne[0])){
-    
+class Intersection{
+    constructor(arrOne, arrTwo){
+        this.arrOne = arrOne
+        this.arrTwo = arrTwo
+        this.newarr = []
+        
+        
+    }
+    getIntersection(){
+        for (let x = 0; x < this.arrOne.length; x++) {
+            if(this.arrTwo.includes(this.arrOne[x])){
+                this.newarr.push(this.arrOne[x])
+            }
+            
+        }
+        console.log(this.newarr)
+    }
 }
+
+const setOne = [43, 65, 21, 65, 76]
+const setTwo = [90, 65, 98, 60, 23]
+const set = new Intersection(setOne, setTwo)
+
+set.getIntersection()
+
